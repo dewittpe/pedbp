020bd93f72e726e79111b515deec0d5b286c4b7a
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-11-20 08:30:01 -0700

    extend lms cpp code

--- src/lms.cpp
+++ src/lms.cpp
@@ -17,22 +17,94 @@ arma::mat cppPGSF(std::string metric, std::string source, int male, double x, do
   // get the needed look up table
   arma::mat LUT;
 
+  if (!(male == 0 || male == 1)) {
+    Rf_error("male needs to be a 0 or 1");
+  }
+
   if (metric == "bmi_for_age") {
     if (source == "WHO") {
       if (male == 1) {
         LUT = bmi_for_age_who_male();
-      } else if (male == 0) {
+      } else {
         LUT = bmi_for_age_who_female();
       }
     } else if (source == "CDC") {
       if (male == 1) {
         LUT = bmi_for_age_cdc_male();
-      } else if (male == 0) {
+      } else {
         LUT = bmi_for_age_cdc_female();
       }
+    } else {
+      Rf_error("Unknown source for bmi_for_age data");
+    }
+  } else if (metric == "head_circumference_for_age_") {
+    if (source == "WHO") {
+      if (male == 1) {
+        //LUT = head_circumference_for_age_who_male();
+        Rf_warning("head circumference for age who male not yet defined");
+      } else {
+        //LUT = head_circumference_for_age_who_female();
+        Rf_warning("head circumference for age who female not yet defined");
+      }
+    } else if (source == "CDC") {
+      if (male == 1) {
+        LUT = head_circumference_for_age_cdc_male();
+      } else {
+        LUT = head_circumference_for_age_cdc_female();
+      }
+    } else {
+      Rf_error("Unknown source for head_circumference_for_age data");
+    }
+  } else if (metric == "stature_for_age") {
+    if (source == "WHO") {
+      if (male == 1) {
+        LUT = stature_for_age_who_male();
+      } else {
+        LUT = stature_for_age_who_female();
+      }
+    } else if (source == "CDC") {
+      if (male == 1) {
+        LUT = stature_for_age_cdc_male();
+      } else {
+        LUT = stature_for_age_cdc_female();
+      }
+    } else {
+      Rf_error("Unknown source for stature_for_age data");
+    }
+  } else if (metric == "weight_for_age") {
+    if (source == "WHO") {
+      if (male == 1) {
+        LUT = weight_for_age_who_male();
+      } else {
+        LUT = weight_for_age_who_female();
+      }
+    } else if (source == "CDC") {
+      if (male == 1) {
+        LUT = weight_for_age_cdc_male();
+      } else {
+        LUT = weight_for_age_cdc_female();
+      }
+    } else {
+      Rf_error("Unknown source for weight_for_age data");
+    }
+  } else if (metric == "weight_for_stature") {
+    if (source == "WHO") {
+      if (male == 1) {
+        LUT = weight_for_stature_who_male();
+      } else {
+        LUT = weight_for_stature_who_female();
+      }
+    } else if (source == "CDC") {
+      if (male == 1) {
+        LUT = weight_for_stature_cdc_male();
+      } else {
+      }
+        LUT = weight_for_stature_cdc_female();
+    } else {
+      Rf_error("Unknown source for weight_for_stature data");
     }
   } else {
-    Rf_error("Not yet implemented");
+    Rf_error("Unknown metric");
   }
 
   // use a binary search to get the row of LUT needed
