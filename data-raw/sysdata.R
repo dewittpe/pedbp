c14441fd89ff3214b684fbcd2b18ee30c3801382
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-11-22 17:30:56 -0700

    rename files so winblows will not complain

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -7,10 +7,6 @@ who_lms_data <- list.files("./data-raw/who", pattern = ".*.xlsx", full.names = T
 who_lms_data <- setNames(lapply(who_lms_data, readxl::read_excel), basename(who_lms_data))
 who_lms_data <- lapply(who_lms_data, data.table::as.data.table)
 
-who_downloads <- scan("./data-raw/who/download.sh", sep = "\n", what = character())
-
-lapply(who_lms_data, names)
-
 # set common names
 for (i in 1:length(who_lms_data)) {
   if ("Age" %in% names(who_lms_data[[i]])) {
@@ -43,62 +39,15 @@ who_lms_data <- unique(who_lms_data)
 who_lms_data[ , male := as.integer(grepl("boy", file))]
 
 # define the "metric"
-metric_by_file <-
-  list('weight_for_age' =
-       c(  "wfa-girls-zscore-expanded-tables.xlsx?sfvrsn=f01bc813_10"
-         , "wfa-girls-percentiles-expanded-tables.xlsx?sfvrsn=54cfa5e8_9"
-         , "wfa-boys-zscore-expanded-tables.xlsx?sfvrsn=65cce121_10"
-         , "wfa-boys-percentiles-expanded-tables.xlsx?sfvrsn=c2f79259_11"
-         # weight for age 5-19 years (file name is odd, but that is what the
-         # link gave)
-         , "hfa-girls-z-who-2007-exp_7ea58763-36a2-436d-bef0-7fcfbadd2820.xlsx?sfvrsn=6ede55a4_4"
-         , "hfa-boys-z-who-2007-exp_0ff9c43c-8cc0-4c23-9fc6-81290675e08b.xlsx?sfvrsn=b3ca0d6f_4"
-         , "hfa-girls-perc-who2007-exp_6040a43e-81da-48fa-a2d4-5c856fe4fe71.xlsx?sfvrsn=5c5825c4_4"
-         , "hfa-boys-perc-who2007-exp_07eb5053-9a09-4910-aa6b-c7fb28012ce6.xlsx?sfvrsn=97ab852c_4"
-       ),
-       'height_for_age' =
-       c(  "hfa-girls-z-who-2007-exp.xlsx?sfvrsn=79d310ee_2"
-         , "hfa-boys-z-who-2007-exp.xlsx?sfvrsn=7fa263d_2"
-         , "hfa-girls-perc-who2007-exp.xlsx?sfvrsn=7a910e5d_2"
-         , "hfa-boys-perc-who2007-exp.xlsx?sfvrsn=27f20eb1_2"
-       ),
-       'length_for_age' =
-       c(  "lhfa-girls-zscore-expanded-tables.xlsx?sfvrsn=27f1e2cb_10"
-         , "lhfa-girls-percentiles-expanded-tables.xlsx?sfvrsn=478569a5_9"
-         , "lhfa-boys-zscore-expanded-tables.xlsx?sfvrsn=7b4a3428_12"
-         , "lhfa-boys-percentiles-expanded-tables.xlsx?sfvrsn=bc36d818_9"
-       ),
-       'weight_for_height' =
-       c(  "wfh-girls-zscore-expanded-tables.xlsx?sfvrsn=daac732c_8"
-         , "wfh-girls-percentiles-expanded-tables.xlsx?sfvrsn=eb27f3ad_7"
-         , "wfh-boys-zscore-expanded-tables.xlsx?sfvrsn=ac60cb13_8"
-         , "wfh-boys-percentiles-expanded-tables.xlsx?sfvrsn=407ceb43_7"
-       ),
-       'weight_for_length' =
-       c(  "wfl-girls-zscore-expanded-table.xlsx?sfvrsn=db7b5d6b_8"
-         , "wfl-girls-percentiles-expanded-tables.xlsx?sfvrsn=e50b7713_7"
-         , "wfl-boys-zscore-expanded-table.xlsx?sfvrsn=d307434f_8"
-         , "wfl-boys-percentiles-expanded-tables.xlsx?sfvrsn=41c436e1_7"
-       ),
-       'bmi_for_age' =
-       c(  "bfa-girls-zscore-expanded-tables.xlsx?sfvrsn=ae4cb8d1_12"
-         , "bfa-girls-percentiles-expanded-tables.xlsx?sfvrsn=e9395fe_9"
-         , "bfa-boys-zscore-expanded-tables.xlsx?sfvrsn=f8e1fbe2_10"
-         , "bfa-boys-percentiles-expanded-tables.xlsx?sfvrsn=aec7ec8d_9"
-         # bmi for age 5-19 years
-         , "bmi-girls-z-who-2007-exp.xlsx?sfvrsn=79222875_2"
-         , "bmi-boys-z-who-2007-exp.xlsx?sfvrsn=a84bca93_2"
-         , "bmi-girls-perc-who2007-exp.xlsx?sfvrsn=e866c0a0_2"
-         , "bmi-boys-perc-who2007-exp.xlsx?sfvrsn=28412fcf_2"
-       )
-      )
-metric_by_file <-
-  metric_by_file |>
-  lapply(function(x) data.table::data.table(file = x)) |>
-  data.table::rbindlist(idcol = "metric")
+who_lms_data[, metric := data.table::tstrsplit(file, split = "-", keep = 1L)]
+who_lms_data[, metric := data.table::fcase(metric == "bfa", "bmi_for_age",
+                                           metric == "hfa", "height_for_age",
+                                           metric == "lhfa", "length_for_age",
+                                           metric == "wfa", "weight_for_age",
+                                           metric == "wfh", "weight_for_height",
+                                           metric == "wfl", "weight_for_length")]
 
-who_lms_data <-
-  merge(x = who_lms_data, y = metric_by_file, all.x = TRUE, by = "file")
+who_lms_data[, .N, by=metric]
 who_lms_data[, file := NULL]
 
 # because percentiles a zscores where provided in seperate files the metric,
